a8bb9d218d156eeb66c3e8d6927bf28c24af0a66
Blake Feldman <blake.feldman@gmail.com>
2019-06-26 17:19:52 -0500

    second commit

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Requiring our Note and Article models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
 
 // Scraping Tools
 var request = require("request");
